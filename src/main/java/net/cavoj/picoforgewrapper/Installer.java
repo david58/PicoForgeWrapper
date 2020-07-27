6f8b2e3045c6cb30f0caf439160107d1a5f6aa01
Samuel Čavoj <samuel@cavoj.net>
2020-07-27 21:28:57 +0200

    Check artifact hashes if -Dpicomc.verify=true is given

--- src/main/java/net/cavoj/picoforgewrapper/Installer.java
+++ src/main/java/net/cavoj/picoforgewrapper/Installer.java
@@ -6,7 +6,12 @@ import net.minecraftforge.installer.json.Install;
 import net.minecraftforge.installer.json.Util;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
@@ -39,13 +44,49 @@ public class Installer {
         return new PicoClientInstall(install, monitor).runPico(this.librariesDir, this.clientTarget);
     }
 
+    private boolean shouldVerifyHash() {
+        return System.getProperty("picomc.verify", "false").equals("true");
+    }
+
+    private static String getFileHash(File file) throws IOException, NoSuchAlgorithmException {
+        FileInputStream fis = new FileInputStream(file);
+        MessageDigest digest = MessageDigest.getInstance("SHA1");
+
+        byte[] buf = new byte[1024];
+        int n = 0;
+
+        while ((n = fis.read(buf)) != -1) {
+            digest.update(buf, 0, n);
+        }
+        fis.close();
+
+        byte[] hash = digest.digest();
+
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hash) {
+            if ((0xff & b) < 0x10) {
+                sb.append("0").append(Integer.toHexString((0xFF & b)));
+            } else {
+                sb.append(Integer.toHexString(0xFF & b));
+            }
+        }
+
+        return sb.toString();
+    }
+
     private boolean checkLibraries(Install install) {
         Map<String, String> data = install.getData(true);
+        Map<String, String> fileHashes = new HashMap<>();
         for (String key : data.keySet()) {
             String value = data.get(key);
             // We are only interested in artifacts
             if (value.charAt(0) == '[' && value.charAt(value.length() - 1) == ']') {
-                data.put(key, Artifact.from(value.substring(1, value.length() -1)).getLocalPath(this.librariesDir).getAbsolutePath());
+                String hash = data.get(key+"_SHA");
+                String path = Artifact.from(value.substring(1, value.length() -1)).getLocalPath(this.librariesDir).getAbsolutePath();
+                if (hash != null) {
+                    fileHashes.put(path, hash.substring(1, hash.length()-1));
+                }
+                data.put(key, path);
             }
         }
         boolean allGood = true;
@@ -70,13 +111,33 @@ public class Installer {
             artifactFiles.add(data.get("MC_SRG"));
         }
 
+        boolean checkHashes = shouldVerifyHash();
+        if (checkHashes) {
+            System.err.println("PicoForgeWrapper: Verifying artifact hashes");
+        }
+
         // Check artifacts
         for (String path : artifactFiles) {
             File artifact = new File(path);
-            // TODO Maybe check hashes as well. Would be cool if it depended on whether picomc ... --verify was used.
             if (!artifact.exists()) {
                 System.err.println("PicoForgeWrapper: Missing artifact: " + artifact.getName());
                 allGood = false;
+            } else if (checkHashes) {
+                String hash = fileHashes.get(path);
+                if (hash != null) {
+                    try {
+                        String computedHash = getFileHash(artifact);
+                        if (!computedHash.equals(hash)) {
+                            System.err.println("PicoForgeWrapper: Hash mismatch: " + artifact.getName());
+                            System.err.println("PicoForgeWrapper: Not checking other files");
+                            allGood = false;
+                            break;
+                        }
+                    } catch (IOException | NoSuchAlgorithmException e) {
+                        e.printStackTrace();
+                        System.err.println("PicoForgeWrapper: Failed to compute hash");
+                    }
+                }
             }
         }
         return allGood;
